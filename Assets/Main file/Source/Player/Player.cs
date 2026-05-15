97de3fb614c00be8cdcdde7b7fc20deabde114bb
MinCh01-CPU <vivy2056@gmail.com>
2026-05-15 23:45:59 +0700

    TileTraining

---
+++ Assets/Main file/Source/Player/Player.cs
@@ -0,0 +1,27 @@
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+    private float horizontal;
+    [SerializeField] private float speed;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if (horizontal != 0) sprite.flipX = horizontal < 0;
+        rb.linearVelocityX = horizontal * speed;
+        anim.SetFloat("VelocityX", Mathf.Abs(rb.linearVelocityX));
+    }
+}
